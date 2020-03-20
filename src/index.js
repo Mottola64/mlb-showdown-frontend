e74d214ad05df10b0cec1d267013ab248138e146
Michael Mottola <mottola64@gmail.com>
2020-03-20 02:14:16 -0400

    installed packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+// import topicReducer from './reducers/topicReducer';
+import {BrowserRouter as Router} from 'react-router-dom'
 import App from './App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let myStore = createStore(topicReducer, composeEnhancers(applyMiddleware(thunk)))
+
+ReactDOM.render(
+<Provider store={myStore}>
+    <Router>
+      <App />  
+    </Router>
+</Provider>
+    ,
+document.getElementById('root'));
