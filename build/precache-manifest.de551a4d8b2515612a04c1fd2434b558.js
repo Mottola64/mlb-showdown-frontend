f847aa377a7d3d85ec8297821d149d4bb4e206c0
Michael Mottola <mottola64@gmail.com>
2020-07-10 21:14:59 -0400

    updated gitignore

---
+++ build/precache-manifest.de551a4d8b2515612a04c1fd2434b558.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31078950e718d1773a69125784e8ca2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01f748e0959f00fb7adb",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "382d5d0a92a1567cf1b6",
+    "url": "/static/css/main.05999295.chunk.css"
+  },
+  {
+    "revision": "01f748e0959f00fb7adb",
+    "url": "/static/js/2.5023cad0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5023cad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382d5d0a92a1567cf1b6",
+    "url": "/static/js/main.da245ebf.chunk.js"
+  },
+  {
+    "revision": "04de130ca5088e661d33",
+    "url": "/static/js/runtime-main.8844b4c8.js"
+  },
+  {
+    "revision": "9fdfe0500379c17506e6471489cf9073",
+    "url": "/static/media/angels.9fdfe050.svg"
+  },
+  {
+    "revision": "4d97863971929745ebf7ccd7c0bf21b8",
+    "url": "/static/media/astros.4d978639.svg"
+  },
+  {
+    "revision": "47e1ff0d6a3b34b9e3a4c26fd4b7494a",
+    "url": "/static/media/athletics.47e1ff0d.svg"
+  },
+  {
+    "revision": "a2fc20de1a338ba24c1dc71c19bcfb9d",
+    "url": "/static/media/blue-jays.a2fc20de.svg"
+  },
+  {
+    "revision": "f8e235f7daa97e086f6fe351b8416f70",
+    "url": "/static/media/braves.f8e235f7.svg"
+  },
+  {
+    "revision": "e89fdd72ed1fc34f56abbfdef703528f",
+    "url": "/static/media/brewers.e89fdd72.svg"
+  },
+  {
+    "revision": "7e5701fd5cfd5aa5b12c6250660c431b",
+    "url": "/static/media/cardinals.7e5701fd.svg"
+  },
+  {
+    "revision": "18535f1189e71c938bb77b7360d9841f",
+    "url": "/static/media/cubs.18535f11.svg"
+  },
+  {
+    "revision": "74faff7f9d6d412ac0df7632398a844f",
+    "url": "/static/media/devil-rays.74faff7f.svg"
+  },
+  {
+    "revision": "fac71a454c5b869185a54184b2d95b5c",
+    "url": "/static/media/diamondbacks.fac71a45.svg"
+  },
+  {
+    "revision": "c44055c8b4e17412da3cce7299f79cd0",
+    "url": "/static/media/dodgers.c44055c8.svg"
+  },
+  {
+    "revision": "2fa1f86c7782516963d264ed99c92c33",
+    "url": "/static/media/giants.2fa1f86c.svg"
+  },
+  {
+    "revision": "67a9ef6d7a117ddf1f906e6401a3cdd5",
+    "url": "/static/media/indians.67a9ef6d.svg"
+  },
+  {
+    "revision": "1e39954f6e4cd3c8fa14a9fe00729221",
+    "url": "/static/media/mariners.1e39954f.svg"
+  },
+  {
+    "revision": "ea6ccf475f427a87dcfdbc7ca3ee92cd",
+    "url": "/static/media/marlins.ea6ccf47.svg"
+  },
+  {
+    "revision": "4fdf2a22f62b73102706e8ececf6ab89",
+    "url": "/static/media/mets.4fdf2a22.svg"
+  },
+  {
+    "revision": "f05b248e99c5e05bac39063f40aefddc",
+    "url": "/static/media/nationals.f05b248e.svg"
+  },
+  {
+    "revision": "a8ea0ea16fb3df04f1b84f2543dba53c",
+    "url": "/static/media/orioles.a8ea0ea1.svg"
+  },
+  {
+    "revision": "a60f8f1c5543c6ef94eff97151758250",
+    "url": "/static/media/padres.a60f8f1c.svg"
+  },
+  {
+    "revision": "db147b3f82bbb356dea3fad356669992",
+    "url": "/static/media/phillies.db147b3f.svg"
+  },
+  {
+    "revision": "ae1140fa1d43ea5152fe570c001c6f8c",
+    "url": "/static/media/pirates.ae1140fa.svg"
+  },
+  {
+    "revision": "239b09be1c1f015c2e2f4feb9bf06120",
+    "url": "/static/media/rangers.239b09be.svg"
+  },
+  {
+    "revision": "17204282459c9f37e22f1630deb13333",
+    "url": "/static/media/red-sox.17204282.svg"
+  },
+  {
+    "revision": "ef69c269a86be5c05bf0e489c8021c90",
+    "url": "/static/media/reds.ef69c269.svg"
+  },
+  {
+    "revision": "9cba6063d387c741e8fc916f807b22e5",
+    "url": "/static/media/rockies.9cba6063.svg"
+  },
+  {
+    "revision": "c33f5038f31b1bd4429c64e1a8488ebd",
+    "url": "/static/media/royals.c33f5038.svg"
+  },
+  {
+    "revision": "976f1d6b01457335a3875d6a38a4e335",
+    "url": "/static/media/tigers.976f1d6b.svg"
+  },
+  {
+    "revision": "631c19458c9afe9d5d489324b6aef479",
+    "url": "/static/media/twins.631c1945.svg"
+  },
+  {
+    "revision": "c60c968d1221059f11df9af8cde77ed7",
+    "url": "/static/media/white-sox.c60c968d.svg"
+  },
+  {
+    "revision": "85a2a80f2049d189d0c2c7b44c3105bb",
+    "url": "/static/media/yankees.85a2a80f.svg"
+  }
+]);
