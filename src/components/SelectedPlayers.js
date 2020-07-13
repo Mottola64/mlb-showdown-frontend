057c83a981484aec5a2e2c86b0aaec5071076590
Michael Mottola <mottola64@gmail.com>
2020-07-13 19:16:01 -0400

    renderPlayer

--- src/components/SelectedPlayers.js
+++ src/components/SelectedPlayers.js
@@ -27,7 +27,7 @@ class SelectedPlayers extends React.Component {
       <td>{attributes.full_name}</td>
       <td>{attributes.team}</td>
       <td>{attributes.points}</td>
-      <button onClick={() => this.props.removePlayer()}>
+      <button onClick={(player) => this.props.removePlayer(player)}>
         Remove Player
       </button>
     </tr>
